5ef68dd3ca1ece704b3ca9fd0cf841eded11d282
Edwin <ewn.francisco@gmail.com>
2026-02-13 08:21:53 -0300

    incluindo classificacao de IMC

--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -4,13 +4,16 @@ import styles from './FormsImc.module.css'
 const FormsImc = () => {
     const [peso, setPeso] = useState(0);
     const [altura, setAltura] = useState(0);
-    
-    const renderizaResultado = () => {
-        if (peso > 0 && altura > 0) {
-            const imc = peso / (altura * altura);
-            return imc.toFixed(2);
-        }
-        return "";
+
+    const imcCalculado = peso > 0 && altura > 0 ? (peso / (altura * altura)) : 0;
+
+    const getClassificacao = () => {
+        if (imcCalculado < 18.5) return "Abaixo do peso";
+        if (imcCalculado < 24.9) return "Peso normal";
+        if (imcCalculado < 29.9) return "Sobrepeso";
+        if (imcCalculado < 34.9) return "Obesidade grau I";
+        if (imcCalculado < 39.9) return "Obesidade grau II";
+        return "Obesidade grau III"
     }
 
     return (
@@ -20,12 +23,16 @@ const FormsImc = () => {
                     <input className={styles.inputs} type="number" placeholder="Peso" onChange={evento => setPeso(parseFloat(evento.target.value))} />
                     <input className={styles.inputs} type="number" placeholder="Altura" onChange={evento => setAltura(parseFloat(evento.target.value))} />
                 </div>
-                <p>Seu índice de massa corporal é:</p>
-                <p className={styles.resultado}>{renderizaResultado() !== "" && altura > 1 ? `${renderizaResultado()} kg/m²` : ""}</p>
+                {imcCalculado > 0 && altura > 1 && (
+                    <div>
+                        <p className={styles.resultado}>Seu índice de massa corporal é:</p>
+                        <p className={styles.resultado}>{imcCalculado.toFixed(2)} kg/m²</p>
+                        <p className={styles.classficacao}>Classificação: {getClassificacao()}</p>
+                    </div>
+                )}
             </form>
         </div>
     )
-
 }
 
 export default FormsImc
